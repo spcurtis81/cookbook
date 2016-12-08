dd9d2b9e417fd1dba590c43c11b932cce5766feb
Stephen Curtis <ste.stuff@icloud.com>
2016-12-08 14:37:28 +0000

    Basic hello world example in Tkinter

---
+++ python/tkinter_hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Script for Python3.* by Stephen Curtis - 8 Dec 2016
+
+from tkinter import *
+
+
+def main():
+
+    root = Tk()  # Creates new top level widget and assigns to var 'root'
+    Label(root,
+          text="Hello, Tkinter!").pack()  # Creates a label in the 'root' widget and uses the pack method to add it.
+    root.mainloop()
+
+
+if __name__ == "__main__": main()
