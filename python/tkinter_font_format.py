a65cf16de9d4c0950fa58a7f1b2b1d10235d0c65
Stephen Curtis <ste.stuff@icloud.com>
2016-12-13 23:01:55 +0000

    Added justify syntax

--- python/tkinter_font_format.py
+++ python/tkinter_font_format.py
@@ -9,8 +9,9 @@ def main():
 
     root = Tk()  # Creates new top level widget and assigns to var 'root'
     label = ttk.Label(root, text="Hello, Ste!")
-    label.config(background='yellow', foreground='blue')
+    label.config(foreground='blue', background='yellow')
     label.config(font=('Courier', 24, 'bold'))
+    label.config(justify = CENTER)
 
     label.pack()
     root.mainloop()
