c619bfd475b3c00763aa77684a1049ce593b03f3
Stephen Curtis <ste.stuff@icloud.com>
2016-12-18 14:46:23 +0000

    Using spinboxes in tkinter.

---
+++ python/tkinter/tkinter_spinbox.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Script for Python3.* by Stephen Curtis - 18 Dec 2016
+
+# NOTE: Spinbox is not available as a ttk widget.
+
+from tkinter import *
+
+root = Tk()
+
+year = StringVar()
+Spinbox(root, from_=1980 to=2020, textvariable=year).pack()
+
+# Syntax
+# <textvariable>.get() - returns the current value of the spinbox
+# <textvariable>.set('<value>') - inserts a value into the spinbox.
+
+
+
+
+
