566d008f10b8e26796d50a77bf1dc3b9cffd2bb6
Stephen Curtis <ste.stuff@icloud.com>
2016-12-23 21:28:10 +0000

    Demonstrates creating an determinate progress bar in tkinter.

---
+++ python/tkinter/tkinter_progressbar_determinate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Script for Python3.* by Stephen Curtis - 23 Dec 2016
+
+# Demonstrates creating an determinate progress bar in tkinter. This allows the progress bar to take values and
+# display progress of task.
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+progressbar = ttk.Progressbar(root, orient=HORIZONTAL, length=200) # Orient can also be VERTICAL
+# maximum sets top 100% progress and value sets the current status.
+progressbar.config(mode='determinate', maximum=11.0, value=4.2)
+progressbar.pack()
+
+
+root.mainloop()
