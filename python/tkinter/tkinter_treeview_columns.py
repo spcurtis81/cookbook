7a69c0331eb7da91f1031612e64313a05493300d
Stephen Curtis <ste.stuff@icloud.com>
2016-12-24 18:22:35 +0000

    Demonstrates use of columns in the treeview in Tkinter.

---
+++ python/tkinter/tkinter_treeview_columns.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# Script for Python3.* by Stephen Curtis - 24 Dec 2016
+
+# Demonstrates use of columns in the treeview in Tkinter.
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('Treeview Columns')
+
+treeview = ttk.Treeview(root)
+treeview.pack()
+
+# Insert treeview items
+treeview.insert('', '0', 'item1', text='First Item')
+treeview.insert('', '1', 'item2', text='Second Item')
+treeview.insert('', 'end', 'item3', text='Third Item')
+# Insert sub-items
+treeview.insert('item1', '0', 'subitem1', text='First Sub Item')
+treeview.insert('item1', '1', 'subitem2', text='Second Sub Item')
+# Insert image
+logo = PhotoImage(file='python_logo.gif').subsample(10, 10)
+treeview.insert('item2', 'end', 'python', text='Python', image=logo)
+treeview.item('item2', open=True)
+
+# Add a column called 'version'
+# (columns='<col_id>')
+treeview.config(columns='version')
+treeview.column('version', width=50, anchor='center')
+
+# Make main column width (id is automatically set to #0 by Python)
+treeview.column('#0', width=150)
+
+# Give column a heading
+treeview.heading('version', text='Version')
+
+# Set value in version column
+# ('<mainCol_id>', '<setCol_id>', '<value2add>')
+treeview.set('python', 'version', '3.5.1')
+
+# Create treeview tags
+# ('<item_id>', tags=('<tag_name>')
+treeview.item('python', tags='software')
+
+# Format using tag
+treeview.tag_configure('software', background='yellow')
+
+root.mainloop()
