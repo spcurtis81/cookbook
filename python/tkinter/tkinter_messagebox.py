8f76c79efedfa1e43dbb73be7a4a10c91fbb6f6e
Stephen Curtis <ste.stuff@icloud.com>
2016-12-25 00:23:05 +0000

    Demonstrates how to initiate message windows in Tkinter

---
+++ python/tkinter/tkinter_messagebox.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Script for Python3.* by Stephen Curtis - 25 Dec 2016
+
+# Demonstrates how to initiate message windows in Tkinter
+
+from tkinter import messagebox
+
+messagebox.showinfo(title='A Friendly Message', message='Hello Tkinter')
