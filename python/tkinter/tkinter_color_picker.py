4a4e3dddc98163511d730f654fd6f568f63ea579
Stephen Curtis <ste.stuff@icloud.com>
2016-12-28 11:05:35 +0000

    Demonstrates how you can use Tkinter's file dialog method to initiate the colour picker

---
+++ python/tkinter/tkinter_color_picker.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Script for Python3.* by Stephen Curtis - 25 Dec 2016
+
+# Demonstrates initiation of the color picker and return values
+
+from tkinter import colorchooser
+
+colorchooser.askcolor(initialcolor='#FFFFFF')
